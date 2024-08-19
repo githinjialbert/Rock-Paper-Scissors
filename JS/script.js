0f998b7c9800107749f2b9f60d22e91f4545aa28
githinjialbert <githinjialbert001@gmail.com>
2024-08-19 10:09:41 +0300

    an update to the function which tells us who won the round

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,40 @@
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorBtn = document.getElementById("scissors");
+const playerScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+const resultMsg = document.getElementById("results-msg");
+const winnerMsg = document.getElementById("winner-msg");
+const resetBtn = document.getElementById("reset-game-btn");
+const gameOptions = document.querySelector(".choice-container");
 
-const getRandomResults = () => {
+
+const pickForComputer = () => {
   const options = ["rock", "paper", "scissors"];
   const randomResult = Math.floor(Math.random() * options.length);
   return options(randomResult);
 }
 
-const whoWonTheRound = (player, computer) => {
-    if(player === "rock" && computer === "scissors" 
-        || player === "paper" && computer === "scissors" 
-        || player === "scissors" && computer === "paper") {
 
-    }
+const didPlayerWin = (player, computer) => {
+    return (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "scissors") ||
+    (player === "scissors" && computer === "paper") 
 }
 
 let playerScore = 0;
 let computerScore = 0;
+
+const findOutWhoWon = (playerChoice) => {
+  const computerChoice = pickForComputer();
+  if(didPlayerWin(playerChoice, computerChoice)) {
+    playerScore++;
+    return `Player wins! ${playerChoice} beats ${computerChoice}`;
+  } else if (playerChoice === computerChoice){
+    return `Its a tie! Both chose ${playerChoice}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerChoice} beats ${playerChoice}`;
+  }
+}
+
